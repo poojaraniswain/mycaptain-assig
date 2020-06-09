90e092c5b6f91b0d3c75214ec6b95438d80f0b3e
poojaraniswain <63733028+poojaraniswain@users.noreply.github.com>
2020-06-09 05:27:17 -0400

    Update positive_number.py

--- positive_number.py
+++ positive_number.py
@@ -1,5 +1,7 @@
 n=int(input("enter the number of items"))
 lst=list(map(int,input("enter the number").strip().split()))[:n]
-for item in lst:
-  if(item>0):
-    print(item,end='')
+lst1=[]
+for a in lst:
+	if(a>0):
+		lst1.append(a)
+print(lst1)
