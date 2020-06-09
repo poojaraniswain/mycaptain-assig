3c6ec1d927c48351886554b054fa61ffa5a11100
poojaraniswain <63733028+poojaraniswain@users.noreply.github.com>
2020-06-09 05:33:02 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,13 @@
 n=int(input("eenter the number of elements"))
 a,b=0,1
-print(a,b)
-for i in range(n):
-  c=a+b
-  print(c,end='')
-  a=b
-  b=c
+if(n==0):
+  print(a)
+elif(n==1):
+  print(a,b)
+else:
+  print(a,b)
+  for i in range(n):
+    c=a+b
+    print(c,end='')
+    a=b
+    b=c
